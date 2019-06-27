41aebdafc75205bc1005529cd2df60b839c5ab48
everaldogoncalves <everaldogeneral@gmail.com>
2019-06-27 20:17:27 +0100

    file home.php added

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+    !--Lang to select tipe of language I'm using-->
+  <html lang="#">
+<head>
+    !--Charset to select tipe of character I'm using-->
+    <meta charset="utf-8"/>
+    <title>Fundamentals Site</title>
+</head>
+<body>
+	 <header>
+	    <nav>
+	        <ul>
+	            <li>Home</li>
+	            <li>Contato</li>
+	        </ul>
+	    </nav>
+	</header>
+
+	<footer>
+	    <p>Copyright 2015 Código Fonte©</p>
+	</footer>
+</body>
+</html>
